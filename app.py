fa1d4766f6b35e39f0cee141f559dd3d0e4ed287
isabellagarciawork-collab <isabellagarciawork@gmail.com>
2026-02-02 00:57:05 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streallit as st
+
+st.set_page_config(layout="wide",page_title="Prueba de Dashboard")
+
+st.title("âœ“ â¡Success!?")
+
+st.success("Si estÃ¡s viendo este mensaje, significa que el entorno de despliegue funciona correctamente.).
+
+st.info("Ahora podemos proceder a restaurar el cÃ³digo completo del dashboard. Por favor, confÃ­rmame que vws este mensaje [ˆH\XØXÚpìÛˆHÝ™X[[]ÛÝYˆ
